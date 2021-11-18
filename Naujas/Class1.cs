ded6acaa58a132bab2eee62abbe5c94f42ee0933
lepaulele <treikauskaite.paulina@gmail.com>
2021-11-18 15:21:44 +0200

    commit

---
+++ Naujas/Class1.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Naujas
+{
+    public class Class1
+    {
+        [Test]
+        public static void TestIf4IsEven()
+        {
+            int leftover = 4 % 2;
+            Assert.AreEqual(0, leftover, "4 is not even");
+        }
+    }
+}
